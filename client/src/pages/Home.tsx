3c2e55a3da6532b605125016d778bd315b860539
swaparup36 <swaparup36@gmail.com>
2025-04-11 03:12:52 +0530

    3D animation added on home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,13 +4,25 @@ import Specials from '~/components/Home/Specials'
 import GetStarted from '~/components/Home/GetStarted'
 import styles from '~/components/Home/styles.module.css'
 import SplashCursor from '../components/SplashCursor'
+import Phoenix from '~/components/MokhshaBird'
+import { useState } from 'react'
+
 export function Component() {
+  const [showContent, setShowContent] = useState(false)
+  const [isAnimating, setIsAnimating] = useState(true)
+
+  const handleComplete = () => {
+    setShowContent(true)
+    setIsAnimating(false)
+  }
+
   return (
     <>
       <Helmet>
         <title>MOKSHA IX | Home</title>
       </Helmet>
-      <main className={styles['home']}>
+      <main className={`${styles['home']} ${isAnimating ? styles['animating'] : ''}`}>
+        <Phoenix onComplete={handleComplete} />
         <SplashCursor></SplashCursor>
         <Hero />
         <Specials />
